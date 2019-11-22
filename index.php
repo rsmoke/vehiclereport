b4fe7eb73af94cf607f3085a323cff86734ecdb4
Margarita Barvinok <brita@umich.edu>
2019-11-22 14:52:48 -0500

    Fix image resize(rotation) function

--- index.php
+++ index.php
@@ -376,13 +376,14 @@ function uploadImageFile($image, $vehiclenum, $uniquename) {
 		$todayDate = date('Ymd');
 		$temp = explode(".", $_FILES[$image]["name"]);
 		$newfilename = $todayDate . $vehiclenum . $uniquename . $image . round(microtime(true)) . '.' . end($temp);
+		$newsizefilename = "admin/uploads/" . $todayDate . $vehiclenum . $uniquename . $image . round(microtime(true)) . '.' . end($temp);
    // resize image before uploading 
                 $sourcefile = $_FILES[$image]["tmp_name"];
-                $max_width = 400;
-                $max_height = 400;
+                $max_width = 800;
+                $max_height = 800;
                 $type = $_FILES[$image]['type'];
                 $endfile = "/Users/brita/Documents/images-cars/temp_file.jpg";
-                ResizeImageFile($sourcefile, $max_width, $max_height, $newfilename, $type);
+                ResizeImageFile($sourcefile, $max_width, $max_height, $newsizefilename, $type);
 //		move_uploaded_file($_FILES[$image]["tmp_name"], "admin/uploads/" . $newfilename);
 // 		rename($endfile, "admin/uploads/" . $newfilename);
 
@@ -404,10 +405,9 @@ function ResizeImageFile($sourcefile, $max_width, $max_height, $endfile, $type) 
                 default :
                 return 'Un supported format';
     }
-
- //   $width = imagesx($img);
- //   $height = imagesy($img);
-        list($width, $height) = getimagesize($sourcefile);
+    $img= ImageFixOrientation($img, $sourcefile);
+    $width = imagesx($img);
+    $height = imagesy($img);
   // count new width and height
     if ($width > $height) {
         if ($width < $max_width) {
@@ -431,7 +431,6 @@ function ResizeImageFile($sourcefile, $max_width, $max_height, $endfile, $type) 
    }
    // Create a new temporary image.
    $tmpimg = imagecreatetruecolor( $newwidth, $newheight );
-
     imagealphablending($tmpimg, false);
     imagesavealpha($tmpimg, true);
     // Copy and resize old image into new image.
@@ -448,13 +447,31 @@ function ResizeImageFile($sourcefile, $max_width, $max_height, $endfile, $type) 
                 imagegif($tmpimg, $endfile, 0);
                 break;
      }
-
    // release the memory
    imagedestroy($tmpimg);
    imagedestroy($img);
    return $endfile;
-
 } // function ResizeImageFile
+function ImageFixOrientation($image, $filename) {
+    $exif = exif_read_data($filename);
+    if (!empty($exif['Orientation'])) {
+        switch ($exif['Orientation']) {
+            case 3:
+                $image = imagerotate($image, 180, 0);
+                break;
+            case 6:
+                $img = imagerotate($image, -90, 0);
+                break;
+            case 8:
+                $image = imagerotate($image, 90, 0);
+                break;
+            default:
+                return $image;
+        }
+        return $img;
+    }
+    return $image;
+} //ImageFixOrientation
 ?>
 
     <?php include("_footer.php"); ?>
